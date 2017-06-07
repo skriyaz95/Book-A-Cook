a929fd92a6775fd208a819747d77a474e69efdbe
skriyaz95 <riyaz.kvgb@gmail.com>
2017-06-07 21:11:13 +0530

    First Commit

---
+++ Book A cook/src/main/java/com/example/index/IndexController.java
@@ -0,0 +1,14 @@
+package com.example.index;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/main")
+	public String indexs()
+	{
+		return "index.html";
+	}
+}
